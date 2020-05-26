1a39d3009ee696db95b24090d0b886c2e8400880
Noah Makau <noshmak@gmail.com>
2020-05-26 09:36:02 -0700

    add day calculator.

--- js/script.js
+++ js/script.js
@@ -27,5 +27,15 @@ function getInput() {
 }
 //calculation func
 function calculateDay(){
+    getInput()
+    dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
+    console.log(dayOfWeek); //Test the calculateDay function
+    return (Math.floor(dayOfWeek));
+    if (dayOfWeek < 0) {
+      dayOfWeek = dayOfWeek * -1;
+    }
+    else if (dayOfWeek > 0) {
+      return dayOfWeek;
+    }
 
 }
